85494bc787217d154960178704cce910faacb97b
Mit Patel <mitpatel2752@gmail.com>
2023-12-27 17:06:38 -0500

    Added Hero Component

---
+++ src/components/pages/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "../Hero.css";
+import HeroImage from '../images/student_learning.png'; // Tell Webpack this JS file uses this image
+
+const Hero = () => {
+    const navigate = useNavigate();
+
+    const handleNavigate = (path) => {  
+      // Navigate to the specified path
+      navigate(path);
+    };
+
+    return (
+        <div className="row">
+            <div className="left-column">
+                <h2 className="left-heading">Your smart note taking solution</h2>
+                <p className="left-paragraph">StudyAI is not just a tool; it's your ally in efficient learning. Elevate your academic experience and focus on understanding, while we handle the notes. </p>
+                <button className="get-started-btn" onClick={() => handleNavigate("/login")}>Get Started</button>
+            </div>
+            <div className="right-column">
+                <img src={HeroImage} alt="Student Learning"/>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import Navbar from "../components/pages/NavBar.js";
+import Hero from "../components/pages/Hero.js";
 
 export default function HomePage() {
   return (
     <Container>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </Container>
   );
 }
